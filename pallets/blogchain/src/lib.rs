383d7771a49379db112cd6c1df27da9ac17072e4
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-02-13 17:50:16 +0100

    add currency

--- pallets/blogchain/src/lib.rs
+++ pallets/blogchain/src/lib.rs
@@ -16,7 +16,7 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{pallet_prelude::*, traits::Currency};
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::pallet]
@@ -28,6 +28,7 @@ pub mod pallet {
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+		type Currency: Currency<Self::AccountId>;
 	}
 
 	// The pallet's runtime storage items.
